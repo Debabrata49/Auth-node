184a4f5d2483aaf517f0e665fcc53102add043a1
Debabrata Karmakar <debabrata.myewards@gmail.com>
2025-07-27 00:54:57 +0530

    changes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,9 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+   res.json({ message: 'Welcome to the Auth API!' });
+});
 router.post('/api/auth/register', register);
 router.post('/api/auth/login', login);
 router.get('/api/auth/verify', authMiddleware, verifyToken);
